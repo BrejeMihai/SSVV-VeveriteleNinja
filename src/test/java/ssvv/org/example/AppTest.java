9d19cc1807456aafd848145e56bc830a19d01e3d
Breje Mihai <brejemihaipaul@gmail.com>
2021-03-08 13:15:41 +0200

    First commit.

---
+++ src/test/java/ssvv/org/example/AppTest.java
@@ -0,0 +1,33 @@
+package ssvv.org.example;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void test1()
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test2()
+    {
+        assertFalse(false);
+    }
+}
