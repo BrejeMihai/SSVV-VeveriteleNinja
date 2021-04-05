b8b5ba5c1b01c8526e218bcc78adee8845cf3ec5
Breje Mihai <brejemihaipaul@gmail.com>
2021-04-05 12:32:28 +0300

    Added service initialization for integration test class.

--- src/test/java/ssvv/org/example/BigBangTest.java
+++ src/test/java/ssvv/org/example/BigBangTest.java
@@ -1,8 +1,30 @@
 package ssvv.org.example;
 
 import org.junit.Test;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.StudentValidator;
+import validation.TemaValidator;
 
 public class BigBangTest {
+
+    public StudentXMLRepository studentXMLRepository;
+    public StudentValidator studentValidator;
+    public Service service;
+
+    public TemaXMLRepository temaXMLRepository;
+    public TemaValidator temaValidator;
+
+    public BigBangTest() {
+        studentValidator = new StudentValidator();
+        studentXMLRepository = new StudentXMLRepository(studentValidator, "appTest.xml");
+
+        temaValidator = new TemaValidator();
+        temaXMLRepository = new TemaXMLRepository(temaValidator, "appTest2.xml");
+
+        service = new Service(studentXMLRepository, temaXMLRepository, null);
+    }
 
     @Test
     public void testAddStudent() {
