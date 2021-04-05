74eb6b7d44a3371df7c5ba0392043b9f6862b7c6
Breje Mihai <brejemihaipaul@gmail.com>
2021-04-05 12:25:42 +0300

    Boilerplate IntegrationTest class.

---
+++ src/test/java/ssvv/org/example/BigBangTest.java
@@ -0,0 +1,26 @@
+package ssvv.org.example;
+
+import org.junit.Test;
+
+public class BigBangTest {
+
+    @Test
+    public void testAddStudent() {
+
+    }
+
+    @Test
+    public void testAddAssignment() {
+
+    }
+
+    @Test
+    public void testAddGrade() {
+
+    }
+
+    @Test
+    public void testBangBang() {
+
+    }
+}
